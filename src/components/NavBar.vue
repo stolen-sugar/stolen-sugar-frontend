1944dbc90ff43cfaab75dc076dcbeaf271a3434e
Ronald Walker <ron22@hey.com>
2022-04-03 09:59:08 -0700

    update site title

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -65,4 +65,12 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.is-primary {
+  background: transparent !important;
+}
+
+.is-primary:active {
+  background: black !important;
+}
+</style>
